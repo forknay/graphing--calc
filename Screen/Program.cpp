869421614b4dad1f8ceeb0add685b4f9fe5b8aa1
Thomas Lewis <110793315+Daesere@users.noreply.github.com>
2024-10-16 01:03:45 -0400

    Display

---
+++ Screen/Program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Window.h"
+
+int main()
+{
+    std::cout << "Creating Window\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+    while (running)
+    {
+        if (pWindow->ProcessMessages())
+        {
+            std::cout << "Closing Window";
+            running = false;
+        }
+
+        //render
+
+        Sleep(10);
+    }
+
+    return 0;
+}
